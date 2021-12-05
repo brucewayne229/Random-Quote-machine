cc01bcafb0557710387c9ed3b67d97e961dc367a
Alex <alexnguyen123229@gmail.com>
2021-12-05 21:16:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import Header from './components/Header';
+import ColorPicker from './components/ColorPicker/';
+import QuoteBox from './components/QuoteBox';
+import Tags from './components/Tags';
+import './App.css'
+
+const colors = [
+  "cornflowerblue",
+  "blueviolet",
+  "indianred",
+  "deeppink",
+  "forestgreen",
+  "red",
+]
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+        activeColor: colors[0],
+    }
+}
+
+handleActiveColor = (newColor) => {
+    this.setState({
+        activeColor: newColor
+    })
+}
+
+  render() {
+    const { activeColor } = this.state;
+
+    return (
+      <div className="App" style={{ backgroundColor:activeColor }}>
+        <div className="Header-box">
+          <Header title="Ramdom quote machine!" />
+        </div>
+
+        <div className="Quote-wrapper">
+          <QuoteBox activeColor={activeColor}/>
+        </div>
+
+        <div className="ColorPicker-box">
+          <ColorPicker 
+            colors={colors} 
+            activeColor={activeColor}
+            handleActiveColor={this.handleActiveColor}
+            />
+        </div>
+
+        <div className="Tags-box">
+          <Tags
+
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
